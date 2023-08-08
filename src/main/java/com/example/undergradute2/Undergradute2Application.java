aa71c96ee0de10532813641853470fd5a7122f13
Evgenii.Pichugov <eugepic@yandex.ru>
2023-08-08 13:26:10 +0300

    initial commit

---
+++ src/main/java/com/example/undergradute2/Undergradute2Application.java
@@ -0,0 +1,13 @@
+package com.example.undergradute2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Undergradute2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Undergradute2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/undergradute2/Undergradute2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.undergradute2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Undergradute2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
